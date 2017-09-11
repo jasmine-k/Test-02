50042a1341b6d39f04366cf816cc1087d5cc9de2
jasmine <jasmine.kaur@knoldus.in>
2017-09-11 17:04:02 +0530

    Test-01

---
+++ project/Dependencies.scala
@@ -0,0 +1,10 @@
+import sbt._
+object Dependencies {
+
+  val commonDependencies: Seq[ModuleID] = Seq(
+    "org.scalatest" %% "scalatest" % "3.0.0" % "test",
+    "org.mindrot" % "jbcrypt" % "0.4"
+
+  )
+
+}
